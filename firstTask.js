ec3c3168a81f6e143f686a37ff8d625f2e815dec
unknown <samsonnnamdi88@gmail.com>
2020-03-24 13:01:40 +0100

    First JS task

--- firstTask.js
+++ firstTask.js
@@ -4,6 +4,8 @@ const courses = ["HTML","CSS", "Javascript","Flutter","c#"]
 
 console.log(`My name is ${name} and I offer the following courses: ${courses}`);
 
+//The number of courses are odd so I display all odd numbers from 1-200
+
 if(courses.length % 2!==0){
     for(i=0; i<=200; i++){
         if(i%2!==0){
