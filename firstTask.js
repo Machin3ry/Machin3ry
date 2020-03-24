0681c3daafcc2fd62ab012b3d326ec80e7df3553
unknown <samsonnnamdi88@gmail.com>
2020-03-24 12:51:00 +0100

    first JS task

---
+++ firstTask.js
@@ -0,0 +1,20 @@
+const name = "Chukwuanieze Samson";
+
+const courses = ["HTML","CSS", "Javascript","Flutter","c#"]
+
+console.log(`My name is ${name} and I offer the following courses: ${courses}`);
+
+if(courses.length % 2!==0){
+    for(i=0; i<=200; i++){
+        if(i%2!==0){
+            console.log(i);
+            
+        }
+    
+    }
+}
+
+else{
+    console.log("The array is even");
+    
+}
